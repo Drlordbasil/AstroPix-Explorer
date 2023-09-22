e2424611655733d3c0da5adc28ee2a24a3f393d7
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-21 20:39:48 -0400

    Enhanced code using GitHubGodFatherv1

--- main.py
+++ main.py
@@ -50,6 +50,47 @@ class ImageFetcher:
             logging.error(f"Error saving image {url}: {str(e)}")
 
 
+class VideoFetcher:
+    def __init__(self, source_urls):
+        self.source_urls = source_urls
+
+    def fetch_video_urls(self):
+        video_urls = []
+        for url in self.source_urls:
+            try:
+                response = requests.get(url)
+                if response.status_code == 200:
+                    soup = BeautifulSoup(response.content, "html.parser")
+                    videos = soup.find_all("video", src=True)
+                    for video in videos:
+                        video_url = video["src"]
+                        if video_url.startswith("http"):
+                            video_urls.append(video_url)
+            except requests.RequestException as e:
+                logging.error(
+                    f"Error fetching video URLs from {url}: {str(e)}")
+        return video_urls
+
+    def save_videos(self, video_urls, save_folder):
+        if not os.path.exists(save_folder):
+            os.makedirs(save_folder)
+
+        with ThreadPoolExecutor() as executor:
+            for i, url in enumerate(video_urls):
+                executor.submit(self.save_video, url, save_folder, i)
+
+    def save_video(self, url, save_folder, index):
+        try:
+            response = requests.get(url, stream=True)
+            if response.status_code == 200:
+                video_path = os.path.join(save_folder, f"video_{index}.mp4")
+                if not os.path.exists(video_path):
+                    with open(video_path, "wb") as f:
+                        shutil.copyfileobj(response.raw, f)
+        except requests.RequestException as e:
+            logging.error(f"Error saving video {url}: {str(e)}")
+
+
 class CosmicImage:
     def __init__(self, image_path):
         self.image_path = image_path
@@ -67,64 +108,6 @@ class CosmicImage:
             print("Image deleted.")
         else:
             print("Image not found.")
-
-
-class CosmicExplorer:
-    def __init__(self, image_folder):
-        self.image_folder = image_folder
-        self.source_urls = [
-            # Replace with real-world URLs or datasets
-            "https://example.com/archivepix.html",
-            "https://example.com/top100/",
-        ]
-
-    def fetch_and_update_images(self):
-        fetcher = ImageFetcher(self.source_urls)
-        image_urls = fetcher.fetch_image_urls()
-        fetcher.save_images(image_urls, self.image_folder)
-
-    def display_random_image(self):
-        images = os.listdir(self.image_folder)
-        if images:
-            random_image = random.choice(images)
-            image_path = os.path.join(self.image_folder, random_image)
-            cosmic_image = CosmicImage(image_path)
-            cosmic_image.display_image()
-        else:
-            print("No images found.")
-
-    def cleanup_outdated_images(self, days=30):
-        current_time = time.time()
-        for file_name in os.listdir(self.image_folder):
-            file_path = os.path.join(self.image_folder, file_name)
-            if os.path.isfile(file_path):
-                creation_time = os.path.getctime(file_path)
-                if current_time - creation_time > days * 24 * 60 * 60:
-                    os.remove(file_path)
-
-    def delete_unused_images(self):
-        images = set(os.listdir(self.image_folder))
-        used_images = set()
-        for url in self.source_urls:
-            response = requests.get(url)
-            if response.status_code == 200:
-                soup = BeautifulSoup(response.content, "html.parser")
-                for img in soup.find_all("img", src=True):
-                    image_url = img["src"]
-                    if image_url.startswith("http"):
-                        image_name = image_url.split("/")[-1]
-                        used_images.add(image_name)
-        unused_images = images - used_images
-        for unused_image in unused_images:
-            os.remove(os.path.join(self.image_folder, unused_image))
-
-    def run(self):
-        logging.basicConfig(filename='image_fetcher.log', level=logging.ERROR,
-                            format='%(asctime)s - %(levelname)s - %(message)s')
-        self.fetch_and_update_images()
-        self.display_random_image()
-        self.cleanup_outdated_images()
-        self.delete_unused_images()
 
 
 class CosmicVideo:
